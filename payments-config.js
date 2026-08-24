fe389a760c40e3ef7ca3df39ab003eb81a28d877
hakdost2006-ops <hakdost2006@gmail.com>
2026-08-24 12:44:15 -0400

    Add retreat Stripe payment link setting

--- payments-config.js
+++ payments-config.js
@@ -3,5 +3,8 @@ const oshoCentralPaymentLinks = {
   oneTimeSupport: "",
   foundingMember: "",
   centerPageSupport: "",
+  // Create a Stripe Payment Link with "Customers choose what to pay" enabled,
+  // then paste the public https://buy.stripe.com/... link here.
+  retreatContribution: "",
   contactEmail: "oshonc369@gmail.com"
 };
